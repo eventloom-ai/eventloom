be4dc898b0bf513e43eec3ca0123134f4aa55347
Yousef Abdelhadi <mmax20920@gmail.com>
2026-08-09 03:15:12 -0400

    Make Eventloom logo transparent

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,8 +8,8 @@ type LogoProps = {
 
 export function EventloomMark({ className = "size-8" }: { className?: string }) {
   return (
-    <span className={["relative inline-block shrink-0 overflow-hidden rounded-[28%] bg-black", className].join(" ")} aria-hidden="true">
-      <Image src="/brand/eventloom-envelope-mark.png" alt="" fill sizes="32px" className="object-cover" />
+    <span className={["relative inline-block shrink-0 drop-shadow-[0_2px_4px_rgba(5,53,70,0.18)]", className].join(" ")} aria-hidden="true">
+      <Image src="/brand/eventloom-envelope-mark-sticker.png" alt="" fill sizes="32px" className="object-contain" />
     </span>
   );
 }
